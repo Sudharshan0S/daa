c991ca886642be7ed0370731a90562670a18763d
Sudharshan.S <sudharshan7795@gmail.com>
2025-04-10 12:07:41 +0530

    Update 04.topologicalsort.java

--- 04.topologicalsort.java
+++ 04.topologicalsort.java
@@ -1,39 +1,42 @@
-import java.util.Scanner;0
-public class topsorts {
-    static int n;
-    static int[][] cost = new int[10][10];
-    static int[] indegree = new int[10];
-    static boolean[] removed = new boolean[10];
-    static void calculate() 
-    {
-        for (int i = 0; i < n; i++) 
+import java.util.Scanner;
+public class Topsort {
+static int cost[][]=new int [10][10];
+static int indegree[]=new int [10];
+static int n;
+static void calculate(){
+      for (int i = 0; i < n; i++){ 
             indegree[i] = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (cost[i][j] == 1) 
-                  indegree[j]++;
+         for (int j = 0; j < n; j++){
+               indegree[i]+=cost[j][i];
+ }   
+}
+}
+static void sourceremoval(){
+    int removed[]=new int [10];
+    System.out.println("topological sort");
+    for(int i=0;i<n;i++){
+    calculate();
+    int j;
+    for(j=0;j<n;j++){
+    if(removed[j]==0&&indegree[j]==0)
+        break;
+    if(j==n){
+       System.out.println("no solution");
+       return;
+                
     }
-    static void sourceRemoval() {
-        System.out.println("Topological Order:");
-        for (int count = 0; count < n; count++) {
-            calculate();
-            int source = -1;
-            for (int i = 0; i < n; i++)
-                if (!removed[i] && indegree[i] == 0) {
-                    source = i;
-                    break;
-                }
-            if (source == -1) {
-                System.out.println("Graph is cyclic, no solution.");
-                return;
-            }
-            System.out.print(source + " ");
-            removed[source] = true;
-            for (int k = 0; k < n; k++) cost[source][k] = 0;
-        }
-        System.out.println();
     }
+    System.out.println(j+" ");
+    removed[j]=1;
+    for(int k=0;k<n;k++){
+        cost[j][k]=0;
+    }
+ 
+    
+    }
+}
     public static void main(String[] args) {
+        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of vertices: ");
         n = sc.nextInt();
@@ -41,6 +44,7 @@ public class topsorts {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 cost[i][j] = sc.nextInt();
-        sourceRemoval();
+        sourceremoval();
     }
-}
+    
+            }
